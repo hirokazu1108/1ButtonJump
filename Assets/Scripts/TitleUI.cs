23ada324b51e4a21bba91ef41f3292cdbc29ac03
hirokazu1108 <k.1108.hiro@gmail.com>
2023-09-24 19:46:50 +0900

    調整

--- Assets/Scripts/TitleUI.cs
+++ Assets/Scripts/TitleUI.cs
@@ -8,9 +8,17 @@ public class TitleUI : MonoBehaviour
     private AudioManager audioManager;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider seSlider;
+    [SerializeField] private GameObject player;
+    private Rigidbody rb = null;
+
+    float time = 0;
+    float preTime =0;
+    int mode = 0;
+    [SerializeField] Vector3[] spawnPos; 
 
     private void Start()
     {
+        rb = player.GetComponent<Rigidbody>();
         audioManager = GameObject.FindObjectsOfType<AudioManager>()[0];
         musicSlider.value = audioManager.bgmAudioSource.volume;
         seSlider.value = audioManager.seAudioSource.volume;
@@ -19,6 +27,40 @@ public class TitleUI : MonoBehaviour
     {
         changedMusicSlider();
         changedSeSlider();
+    }
+
+    private void FixedUpdate()
+    {
+        time += Time.deltaTime;
+        if (Mathf.Abs(time-preTime) >0.5f)
+        {
+            if(mode == 0)
+            {
+                rb.velocity = new Vector3(Random.Range(1f, 2f), Random.Range(-1f, 2f), Random.Range(-1, 1.5f));
+                preTime = time;
+            }
+            else
+            {
+                rb.velocity = new Vector3(-Random.Range(1f, 2f), Random.Range(-1f, 2f), Random.Range(-1, 1.5f));
+                preTime = time;
+            }  
+        }
+
+        if(player.transform.position.x > 35)
+        {
+            mode = 1;
+            player.transform.position = spawnPos[1];
+            rb.velocity = Vector3.zero;
+            player.transform.rotation = Quaternion.Euler(0, -180f, 0);
+        }
+        if(player.transform.position.x < -35)
+        {
+            mode = 0;
+            player.transform.position = spawnPos[0];
+            rb.velocity = Vector3.zero;
+            player.transform.rotation = Quaternion.Euler(0, 180f, 0);
+        }
+       
     }
     public void changedMusicSlider()
     {
